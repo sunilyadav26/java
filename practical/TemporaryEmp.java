1e901221fdcb7021bacdb2060a723dabd5f2777b
Sunil yadav <109275142+sunilyadav26@users.noreply.github.com>
2022-09-28 22:56:23 +0530

    Add files via upload

---
+++ practical/TemporaryEmp.java
@@ -0,0 +1,12 @@
+package Corejava;
+
+class TemporaryEmp extends Employee {
+	 Double hike=0.35;
+	 public static void main(String args[]) {
+		 TemporaryEmp obj2=new TemporaryEmp();
+		 System.out.println(obj2.salary);
+		 System.out.println(obj2.hike);
+	 }
+	
+
+}
