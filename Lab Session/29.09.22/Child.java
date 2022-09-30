c0c0df3830a4d3e7decdf26d7e6c38ab0d11753b
Sunil yadav <109275142+sunilyadav26@users.noreply.github.com>
2022-09-30 15:19:01 +0530

    Add files via upload

---
+++ Lab Session/29.09.22/Child.java
@@ -0,0 +1,16 @@
+//Inherit parent class
+package Corejava;
+
+    class Child extends Parent {
+    //create method
+    	void speak() {
+    	System.out.println("which song");
+    }
+    	//creating second method
+         void dance() {
+        	 System.out.println("I can't Dance");
+         }
+    
+    
+    
+    }
