f38a63d05b186343570f191eb118d4ca13ec8ffe
Sunil yadav <109275142+sunilyadav26@users.noreply.github.com>
2022-10-07 21:08:48 +0530

    Add files via upload

---
+++ Lab Session/06.10.2022/ReversePyramid.java
@@ -0,0 +1,31 @@
+package CoreJava;
+
+import java.util.Scanner;
+
+public class ReversePyramid {
+
+	public static void main(String[] args) {
+		//create object of Scanner class 
+		Scanner sc=new Scanner(System.in);
+		//taking input from user
+		 System.out.println( "enter any number :");
+		 int a=sc.nextInt();
+		 int s=0;
+		 //using for loop
+		 for(int i=a;i>0;i--) {
+			 //inner loop for printing
+			 for(int j=0;j<i;j++) {
+				 System.out.print("$" );
+				 //for next line
+			 }System.out.println("");
+			 s+=1;
+			 //for use for Space
+			 for(int m=0;m<s;m++) {
+				 System.out.print(" ");
+			 }
+			 i--;
+			 
+			 }
+		 }
+
+}
