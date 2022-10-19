d923decb8597c58b3090175745ba47925b913c21
Sunil yadav <109275142+sunilyadav26@users.noreply.github.com>
2022-10-19 15:23:38 +0530

    Add files via upload

---
+++ Hands on/ArrayString.java
@@ -0,0 +1,15 @@
+package CoreJava;
+
+public class ArrayString {
+
+	public static void main(String[] args) {
+		// declaration and instantiation 
+		String name[]= {"Joshphine","Chris","katherine"};
+		  
+		//	traversing array	
+		for(int i=0;i<name.length;i++) {
+	    System.out.println(name[i]);
+	}
+
+	}
+}
